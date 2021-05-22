6312f186347f71ba6e814bac9e6b2f5e3c3ffebc
Brian Morgan <brianjm2007@yahoo.com>
2021-05-22 13:17:42 -0400

    started readme fixed issues on java.js completed style.cs

--- script.js
+++ script.js
@@ -1,211 +1,182 @@
-window.addEventListener('load', function () {
-  // Grab the existing history from local storage IF it exists
-  var existingHistory;
-  if (!JSON.parse(localStorage.getItem('history'))) {
-    existingHistory = [];
-  } else {
-    existingHistory = JSON.parse(localStorage.getItem('history'));
+// Event listener for the button on the search bar
+  // On click, it will change the CSS to make the containers appear on the page
+  // It will then start the ajax to get weather data using API keys and Open Weather
+  // It looks at the data from the API's we call, which the developer accesses from the console, 
+  // Then uses that data to manipulate CSS elements
+
+
+  // This is a conditional a TA helped me develop, where it parses local storage data into a variable,
+  // If there is no storage available, it begins with an empty array.
+  var cityData = localStorage.getItem("cityData")
+    ? JSON.parse(localStorage.getItem("cityData"))
+    : [];
+
+  // This is a button maker. It runs the local storage data through a loop, storing that data on the button,
+  // Then prepending it onto the list. The buttons are actually list elements, that have an event listener to generate the weather.
+  function generateButton() {
+    $(".hot-times").empty();
+    for(i = 0; i < cityData.length; i++){
+    // var cityEl = $("#city-input").val();
+    var cityButtonEl = $("<li class='list-group-item hot-times new-city'>").text(cityData[i]);
+    cityButtonEl.on("click", function(){
+      generateWeather($(this).text());
+    });
+    $("#city-list").prepend(cityButtonEl);
   }
-
-  var historyItems = [];
-
-  // Function to get the forecast, loop through only the days of the week and render data to the page
-  function getForecast(searchValue) {
-    if (!searchValue) {
-      return;
-    }
-    var endpoint = `http://api.openweathermap.org/data/2.5/forecast?q=${searchValue}&appid=d91f911bcf2c0f925fb6535547a5ddc9&units=imperial`;
-    fetch(endpoint)
-      .then((res) => res.json())
-      .then((data) => {
-        // Select our forecast element and add a header to it
-        var forecastEl = document.querySelector('#forecast');
-        forecastEl.innerHTML = '<h4 class="mt-3">5-Day Forecast:</h4>';
-
-        // Create a div and give it a class of row
-        forecastRowEl = document.createElement('div');
-        forecastRowEl.className = '"row"';
-
-        // Loop over all forecasts (by 3-hour increments)
-        for (var i = 0; i < data.list.length; i++) {
-          // Only look at forecasts around 3:00pm
-          if (data.list[i].dt_txt.indexOf('15:00:00') !== -1) {
-            // Create HTML elements for a bootstrap card
-            var colEl = document.createElement('div');
-            colEl.classList.add('col-md-2');
-            var cardEl = document.createElement('div');
-            cardEl.classList.add('card', 'bg-primary', 'text-white');
-            var windEl = document.createElement('p');
-            windEl.classList.add('card-text');
-            windEl.textContent = `Wind Speed: ${data.list[i].wind.speed} MPH`;
-            var humidityEl = document.createElement('p');
-            humidityEl.classList.add('card-text');
-            humidityEl.textContent = `Humidity : ${data.list[i].main.humidity} %`;
-            var bodyEl = document.createElement('div');
-            bodyEl.classList.add('card-body', 'p-2');
-            var titleEl = document.createElement('h5');
-            titleEl.classList.add('card-title');
-            titleEl.textContent = new Date(
-              data.list[i].dt_txt
-            ).toLocaleDateString();
-            var imgEl = document.createElement('img');
-            imgEl.setAttribute(
-              'src',
-              `http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`
-            );
-            var p1El = document.createElement('p');
-            p1El.classList.add('card-text');
-            p1El.textContent = `Temp: ${data.list[i].main.temp_max} °F`;
-            var p2El = document.createElement('p');
-            p2El.classList.add('card-text');
-            p2El.textContent = `Humidity: ${data.list[i].main.humidity}%`;
-
-            // Merge together and put on page
-            colEl.appendChild(cardEl);
-            bodyEl.appendChild(titleEl);
-            bodyEl.appendChild(imgEl);
-            bodyEl.appendChild(windEl);
-            bodyEl.appendChild(humidityEl);
-            bodyEl.appendChild(p1El);
-            bodyEl.appendChild(p2El);
-            cardEl.appendChild(bodyEl);
-            forecastEl.appendChild(colEl);
-          }
-        }
-      });
-  }
-
-  // Helper function to fetch and display the UV index
-  function getUVIndex(lat, lon) {
-    fetch(
-      `http://api.openweathermap.org/data/2.5/uvi?appid=d91f911bcf2c0f925fb6535547a5ddc9&lat=${lat}&lon=${lon}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        var bodyEl = document.querySelector('.card-body');
-        var uvEl = document.createElement('p');
-        uvEl.id = 'uv';
-        uvEl.textContent = 'UV Index: ';
-        var buttonEl = document.createElement('span');
-        buttonEl.classList.add('btn', 'btn-sm');
-        buttonEl.innerHTML = data.value;
-
-        switch (data.value) {
-          case data.value < 3:
-            buttonEl.classList.add('btn-success');
-            break;
-          case data.value < 7:
-            buttonEl.classList.add('btn-warning');
-            break;
-          default:
-            buttonEl.classList.add('btn-danger');
-        }
-
-        bodyEl.appendChild(uvEl);
-        uvEl.appendChild(buttonEl);
-      });
-  }
-
-  const handleHistory = (term) => {
-    if (existingHistory && existingHistory.length > 0) {
-      var existingEntries = JSON.parse(localStorage.getItem('history'));
-      var newHistory = [...existingEntries, term];
-      localStorage.setItem('history', JSON.stringify(newHistory));
-      // If there is no history, create one with the searchValue and save it localStorage
-    } else {
-      historyItems.push(term);
-      localStorage.setItem('history', JSON.stringify(historyItems));
-    }
   };
 
-  // Function that preforms the actual API request and creates elements to render to the page
-  function searchWeather(searchValue) {
-    var endpoint = `http://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=d91f911bcf2c0f925fb6535547a5ddc9&units=imperial`;
-    fetch(endpoint)
-      .then((res) => res.json())
-      .then((data) => {
-        // Invoke our history method
-        if (!existingHistory.includes(searchValue)) {
-          handleHistory(searchValue);
-        }
-        // Clear any old content
-        todayEl = document.querySelector('#today');
-        todayEl.textContent = ' ';
+  function recallCity() {
+    generateWeather(this.innerHTML);
+  }
 
-        // Create html content for current weather
-        var titleEl = document.createElement('h3');
-        titleEl.classList.add('card-title');
-        titleEl.textContent = `${
-          data.name
-        } (${new Date().toLocaleDateString()})`;
-        var cardEl = document.createElement('div');
-        cardEl.classList.add('card');
-        var windEl = document.createElement('p');
-        windEl.classList.add('card-text');
-        var humidEl = document.createElement('p');
-        humidEl.classList.add('card-text');
-        var tempEl = document.createElement('p');
-        tempEl.classList.add('card-text');
-        humidEl.textContent = `Humidity: ${data.main.humidity} %`;
-        tempEl.textContent = `Temperature: ${data.main.temp} °F`;
-        var cardBodyEl = document.createElement('div');
-        cardBodyEl.classList.add('card-body');
-        var imgEl = document.createElement('img');
-        imgEl.setAttribute(
-          'src',
-          `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
-        );
+  function generateWeather(cityEl) {
 
-        // Append all the content that we created
-        titleEl.appendChild(imgEl);
-        cardBodyEl.appendChild(titleEl);
-        cardBodyEl.appendChild(tempEl);
-        cardBodyEl.appendChild(humidEl);
-        cardBodyEl.appendChild(windEl);
-        cardEl.appendChild(cardBodyEl);
-        todayEl.appendChild(cardEl);
-
-        // Invoke our forecast and UV functions
-        getForecast(searchValue);
-        getUVIndex(data.coord.lat, data.coord.lon);
+    var date = moment().format("(M/D/YYYY)");
+    
+    console.log(cityEl);
+    var apiKey = "1935f5d7d75a269680ddfadd7b264dcb";
+  // Key for general weather
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      cityEl +
+      "&units=imperial&appid=" +
+      apiKey;
+  // Key for five day forecast
+    var fivedayURL =
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      cityEl +
+      "&units=imperial&appid=" +
+      apiKey;
+  
+    // Hello, my name is Ajax, aka Telamonian Ajax, aka Ajax the Great, not to be confused with Ajax the Lesser.
+      // This is the ajax for the general weather forecast for today that appears in the jumbotron
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      // console.log(response);
+    // Caoturing weather data from the API into variables
+      var tempEl = response.main.temp;
+      var humidEl = response.main.humidity;
+      var windEl = response.wind.speed;
+      var iconcode = response.weather[0].icon;
+      var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+      var latEl = response.coord.lat;
+      var lonEl = response.coord.lon;
+    // Would you look at that, 
+    // it's all the weater properties that link to the jumbotron HTML through their ID's
+      $("#jumbo-date").text(response.name + " " + date);
+      $("#jumbo-temp").text("Temperature: " + tempEl);
+      $("#jumbo-humid").text("Humidity: " + humidEl + "%");
+      $("#jumbo-wind").text("Wind: " + windEl + " MPH");
+      $("#jumbo-icon").attr("src", iconurl);
+  
+    // UV Index API
+      var uviURL =
+      "https://api.openweathermap.org/data/2.5/uvi?appid=" +
+      apiKey +
+      "&lat="+
+      latEl +
+      "&lon="+
+      lonEl;
+      // More AJAX, but this time for the UV Index
+      $.ajax({
+          url: uviURL,
+          method: "GET",
+        }).then(function (response) {
+          // console.log(response);
+        // Based on which value is returned from API, this conditional manipulates the HTML elements with a background-color
+          var uviEl = response.value;
+          $("#jumbo-uv").text(uviEl);
+          if(uviEl < 3){
+              $("#jumbo-uv").attr("style", "background-color:green");
+          } else if (uviEl > 3 && uviEl < 6) {
+              $("#jumbo-uv").attr("style", "background-color:orange");
+          } else  {
+              $("#jumbo-uv").attr("style", "background-color:red");
+          }
+          $(".new-city").on("click", recallCity);
       });
-  }
-
-  // Helper function to create a new row
-  function makeRow(searchValue) {
-    // Create a new `li` element and add classes/text to it
-    var liEl = document.createElement('li');
-    liEl.classList.add('list-group-item', 'list-group-item-action');
-    liEl.id = searchValue;
-    var text = searchValue;
-    liEl.textContent = text;
-
-    // Select the history element and add an event to it
-    liEl.addEventListener('click', (e) => {
-      if (e.target.tagName === 'LI') {
-        searchWeather(e.target.textContent);
-      }
     });
-    document.getElementById('history').appendChild(liEl);
+  // You know me, always living in the moment,
+  // This helps the 5 Day Forecast dates look at tomorrow, the next day, etc. by adding days
+  // I honestly felt really smart figuring this out
+    var dayOne = moment().add(1, "days");
+    dayOne = dayOne.format("(M/D/YYYY)");
+    $("#day-one").text(dayOne);
+  
+    var dayTwo = moment().add(2, "days");
+    dayTwo = dayTwo.format("(M/D/YYYY)");
+    $("#day-two").text(dayTwo);
+  
+    var dayThree = moment().add(3, "days");
+    dayThree = dayThree.format("(M/D/YYYY)");
+    $("#day-three").text(dayThree);
+  
+    var dayFour = moment().add(4, "days");
+    dayFour = dayFour.format("(M/D/YYYY)");
+    $("#day-four").text(dayFour);
+  
+    var dayFive = moment().add(5, "days");
+    dayFive = dayFive.format("(M/D/YYYY)");
+    $("#day-five").text(dayFive);
+  
+  // Well, well, well if it isn't all the data for the Five Day Forecast. It's a monster.
+    $.ajax({
+      url: fivedayURL,
+      method: "GET",
+    }).then(function (response) {
+      // console.log(response);
+      var dayOneTemp = response.list[3].main.temp;
+      var dayOneHumid = response.list[3].main.humidity;
+      var dayOneIconCode = response.list[3].weather[0].icon;
+      var dayOneIconURL = "http://openweathermap.org/img/w/" + dayOneIconCode + ".png";
+      var dayTwoTemp = response.list[11].main.temp;
+      var dayTwoHumid = response.list[11].main.humidity;
+      var dayTwoIconCode = response.list[11].weather[0].icon;
+      var dayTwoIconURL = "http://openweathermap.org/img/w/" + dayTwoIconCode + ".png";
+      var dayThreeTemp = response.list[19].main.temp;
+      var dayThreeHumid = response.list[19].main.humidity;
+      var dayThreeIconCode = response.list[19].weather[0].icon;
+      var dayThreeIconURL = "http://openweathermap.org/img/w/" + dayThreeIconCode + ".png";
+      var dayFourTemp = response.list[27].main.temp;
+      var dayFourHumid = response.list[27].main.humidity;
+      var dayFourIconCode = response.list[27].weather[0].icon;
+      var dayFourIconURL = "http://openweathermap.org/img/w/" + dayFourIconCode + ".png";
+      var dayFiveTemp = response.list[35].main.temp;
+      var dayFiveHumid = response.list[35].main.humidity;
+      var dayFiveIconCode = response.list[35].weather[0].icon;
+      var dayFiveIconURL = "http://openweathermap.org/img/w/" + dayFiveIconCode + ".png";
+  // What else can I say? It's pulilng a ton of information from that API and influencing it in the HTML
+      $("#fd-one-temp").text("Temp: "+dayOneTemp);
+      $("#fd-one-humid").text("Humidity: "+dayOneHumid+"%");
+      $("#fd-one-image").attr("src", dayOneIconURL);
+      $("#fd-two-temp").text("Temp: "+dayTwoTemp);
+      $("#fd-two-humid").text("Humidity: "+dayTwoHumid+"%");
+      $("#fd-two-image").attr("src", dayTwoIconURL);
+      $("#fd-three-temp").text("Temp: "+dayThreeTemp);
+      $("#fd-three-humid").text("Humidity: "+dayThreeHumid+"%");
+      $("#fd-three-image").attr("src", dayThreeIconURL);
+      $("#fd-four-temp").text("Temp: "+dayFourTemp);
+      $("#fd-four-humid").text("Humidity: "+dayFourHumid+"%");
+      $("#fd-four-image").attr("src", dayFourIconURL);
+      $("#fd-five-temp").text("Temp: "+dayFiveTemp);
+      $("#fd-five-humid").text("Humidity: "+dayFiveHumid+"%");
+      $("#fd-five-image").attr("src", dayFiveIconURL);
+    });
   }
 
-  // Render existing history to the page.
-  if (existingHistory && existingHistory.length > 0) {
-    existingHistory.forEach((item) => makeRow(item));
-  }
+$("#city-button").on("click", function () {
+  // Function that generates buttons based off text input
 
-  // Helper function to get a search value.
-  function getSearchVal() {
-    var searchValue = document.querySelector('#search-value').value;
-    if (searchValue) {
-      searchWeather(searchValue);
-      makeRow(searchValue);
-      document.querySelector('#search-value').value = '';
-    }
-  }
-
-  // Attach our getSearchVal function to the search button
-  document
-    .querySelector('#search-button')
-    .addEventListener('click', getSearchVal);
+  var cityEl = $("#city-input").val();
+  cityData.push(cityEl);
+  localStorage.setItem("cityData", JSON.stringify(cityData));
+  generateButton();
+  generateWeather(cityEl);
+  
 });
+// This assures the weather remains showing upon refresh
+generateButton();
+var lastCityEl = cityData[cityData.length - 1];
+generateWeather(lastCityEl);
